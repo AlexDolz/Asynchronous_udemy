c45b537d8ba011c3c27174a27b6ad8ab47a127a8
AlexDolz <aleksejdolznikov3@gmail.com>
2023-11-28 15:02:07 +0100

    promise and fetch API

--- script.js
+++ script.js
@@ -84,3 +84,10 @@ const getCountryAndNeighbour = function (country) {
 // getCountryAndNeighbour('portugal');
 // getCountryAndNeighbour('lithuania');
 getCountryAndNeighbour('usa');
+
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+const request = fetch('https://restcountries.com/v2/name/portugal');
+console.log(request);
