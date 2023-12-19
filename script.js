94ec64fdb7b48e5bf179c1955ad2ad7a12e2131b
AlexDolz <aleksejdolznikov3@gmail.com>
2023-12-19 15:52:03 +0100

    Running promises in parallel

--- script.js
+++ script.js
@@ -355,3 +355,27 @@ console.log('FIRST');
 // } catch (err) {
 //   alert(err.message);
 // }
+
+// Running promises in parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    // at the same time
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+
+    console.log(data1.capital, data2.capital, data3.capital);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
